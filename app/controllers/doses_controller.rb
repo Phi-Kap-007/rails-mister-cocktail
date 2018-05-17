fcfd9f29c3d5df1841d39ad67e9550986243d17d
Alex Corrado alexcorrado22@gmail.com <alexcorrado22@gmail.com>
2018-05-17 15:56:46 -0400

    Perfect Functionality, Ugly Design (AKA How I Operate)

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,32 @@
+class DosesController < ApplicationController
+
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.valid?
+      @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def dose_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    params.require(:dose).permit(:ingredient_id, :description, :cocktail_id)
+  end
+end
