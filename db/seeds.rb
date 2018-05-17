71e7f77355ed35d8ffeea1b372346728fa899763
Alex Corrado alexcorrado22@gmail.com <alexcorrado22@gmail.com>
2018-05-17 17:00:11 -0400

    Slightly Better Design

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 cocktails = Cocktail.create([{ name: 'Rhum & Coke' }, { name: 'Gin & Tonic' }, { name: 'Dark & Stormy' }])
-ingredients = ([{ name: 'Rhum' }, { name: 'Coke' }, { name: 'Gin' }, { name: 'Club Soda' }])
-doses = ([{ingredient_id: 1, cocktail_id: 1, description: '1 Cup of Alcohol'}])
+ingredients = Ingredient.create([{ name: 'Rhum' }, { name: 'Coke' }, { name: 'Gin' }, { name: 'Club Soda' }])
